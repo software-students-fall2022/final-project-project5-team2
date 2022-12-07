44d871bdf52a32ace1841f565bcf2a8ab4d98e44
Lucy Kocharian <LK2583@nyu.edu>
2022-12-07 15:38:55 -0500

    set up basic bones of web-app

---
+++ web-app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from dotenv import load_dotenv
+import os
+
+import pymongo
+
+#instantiate the app
+app = Flask(__name__)
+
+#load config options from .env file
+load_dotenv()
+
+#connect to db
+
+
+#routes
+@app.route('/')
+
+#home page
+def home():
+    """
+    Home Page
+    """
+
+# route to handle any errors
+@app.errorhandler(Exception)
+def handle_error(e):
+    """
+    Output errors.
+    """
+    return render_template('error.html', error=e), 404
+
+#run app
+if __name__ == "__main__":
+    PORT = os.getenv('PORT',5000)
+
+    app.run(port=PORT)
