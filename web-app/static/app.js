d1e13e7d7bd64b639de18fa72347ffcb3a4c515e
qlaueen <qlaueen@gmail.com>
2022-12-17 16:31:09 -0500

    camera func

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -19,7 +19,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
     cameraOutput.classList.add("taken");
 };// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
