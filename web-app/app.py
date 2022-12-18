3741dfac6be5cf35a522cd1fa2a350946b7064f1
Pedro <pbaggio765@gmail.com>
2022-12-18 01:20:30 -0500

    Fix buttons in feed

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, session, flash
+from flask import Flask, render_template, request, session, redirect, url_for, make_response, session, flash, Response
+from flask_session import Session
 from dotenv import load_dotenv
 from bson.json_util import dumps, loads, ObjectId
 from mongodb import Database
 from datetime import datetime
 import bcrypt
+
 
 import random
 import os
@@ -11,70 +13,104 @@ import os
 #instantiate the app
 app = Flask(__name__)
 
+app.config['SESSION_TYPE'] = "mongodb"
+app.config['SECRET_KEY'] = os.urandom(24)
+
 #load config options from .env file
 load_dotenv()
 
 #connect to db
 Database.initialize()
 app.config['MONGO_dbname'] = 'users'
-#routes
-@app.route('/')
 
-#home page
-def home():
+#routes
+@app.route('/', methods=['POST', 'GET'])
+def index():
     """
     Home Page
     """
+    # User logs out
+    if request.method == "POST" and "username" in session:
+        session.clear()
+
+    if "username" in session:
+        return redirect(url_for("feed"))
+
     return render_template('welcome.html')
 
-
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        users = mongo.db.users
-        signin_user = users.find_one({'username': request.form['username']})
+    message = 'Please login to your account'
+    if "username" in session:
+        return redirect(url_for("feed"))
 
-        if signin_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), signin_user['password'].encode('utf-8')) == \
-                    signin_user['password'].encode('utf-8'):
-                session['username'] = request.form['username']
-                return redirect(url_for('index'))
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        flash('Username and password combination is wrong')
-
-
+        user_found = Database.find_single('users', {"username": username})
+        if user_found:
+            user_val = user_found['username']
+            passwordcheck = user_found['password']
+            
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["username"] = user_val
+                return redirect(url_for('feed'))
+            else:
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Username not found'
+            return render_template('login.html', message=message)
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        users = mongo.db.users
-        signup_user = users.find_one({'username': request.form['username']})
+        signup_user = Database.find_single('users', {'username': request.form['username']})
+        if signup_user:
+            flash(f"Username {request.form['username']} is already taken")
+            return redirect(url_for('signup'))
 
-    if signup:
-        flash(request.form['username'] + ' username is already exist')
-        return redirect(url_for('signup'))
-
-    hashed = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt(14))
-    users.insert({'username': request.form['username'], 'password': hashed})
-    return redirect(url_for('login'))
+        hashed = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt(14))
+        Database.insert_one('users', {'username': request.form['username'], 'password': hashed})
+        return redirect(url_for('login'))
 
     return render_template("signup.html")
 
 @app.route("/feed")
 def feed():
-    posts = get_all_posts()
+
+    logged_in = "username" in session
+    
     sortby = request.args.get('sortby', '')
+    query = request.args.get('query', '')
+        
+    posts = get_all_posts()
+
+    posts = [post for post in posts if ( query.lower() in post['prompt'].lower())]
     posts = sort_posts(posts, sortby)
     now = datetime.now()
     for post in posts:
         date_posted = post['time_created']
         post['time_since'] = get_time_from(date_posted, now)
-    return render_template("feed.html", posts=posts)
+        
+    return render_template("feed.html", posts=posts, logged_in=logged_in)
+
+@app.route("/post/<id>")
+def see_post(id):
+    post = Database.find_single('photos', id, "_id")
+    print(post)
+    return render_template("post.html", post=post)
+
+
 
 @app.route("/new-post")
 def newPost():
-    return render_template("newPost.html")
+    if "username" not in session:
+        return redirect(url_for("login"))
+        
+    return render_template("newPost.html", prompt=get_random_prompt())
 
 # route to handle any errors
 @app.errorhandler(Exception)
@@ -128,7 +164,48 @@ def get_random_prompt():
     prompt = random.choice(get_all_prompts())
     return prompt
 
+
+# auth
+
+# @app.route("/", methods=['post', 'get'])
+# def index():
+#     # message = ''
+#     # if 'email' in session:
+#     #     return redirect(url_for("logged_in"))
+#     # if request.method == "POST":
+#     #     user = request.form.get("user")
+#     #     email = request.form.get("email")
+
+#     #     password = request.form.get("password")
+#     #     password_confirm = request.form.get("password_confirm")
+
+#     #     user_found = records.find_one({"user": user})
+#     #     email_found = records.find_one({"email": email})
+
+#     #     if user_found:
+#     #         message = "This username is already taken."
+#     #         return render_template('index.html')
+#     #     if email_found:
+#     #         message = "This email already has an account linked to it."
+#     #         return render_template('index.html')
+#     #     if password != password_confirm: 
+#     #         message = "Passwords should match."
+#     #         return render_template('index.html')
+#     #     else:
+#     #         hashed = bcrypt.hashpw(password_confirm.encode('utf-8'), bcrypt.gensalt())
+#     #         user_input = {"user": user, 'email': email, 'password': hashed}
+#     #         records.insert_one(user_input)
+
+#     #         user_data = records.find_one({"email": email})
+#     #         new_email = user_data['email']
+
+#     #         return render_template('feed.html', email = new_email)
+#     return render_template('index.html')
+
+
 #run app
 if __name__ == "__main__":
+
+
     PORT = os.getenv('PORT', 8080)
     app.run(port=PORT)
