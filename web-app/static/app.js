2a00d7e08396a132428f075ba63079e3a03e0faf
ezmartini <em4047@nyu.edu>
2022-12-18 19:56:34 -0500

    Added ability to add new post!

--- web-app/static/app.js
+++ web-app/static/app.js
@@ -1,25 +1,33 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
+var constraints = { video: { facingMode: "user" }, audio: false }; // Define constants
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger"); // Access the device camera and stream to cameraView
+const cameraSubmit = document.querySelector("#camera--submit");
 function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
+    .catch(function (error) {
+      console.error("Oops. Something is broken.", error);
     });
-}// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/png");
-    cameraOutput.classList.add("taken");
-};// Start the video stream when the window loads
+} // Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function (e) {
+  e.preventDefault();
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/png");
+  cameraOutput.classList.add("taken");
+
+  cameraView.style = "display:none;";
+  cameraTrigger.style = "display:none;";
+  cameraSubmit.style = "display:block;";
+
+  document.getElementById("opImage").value = cameraOutput.src;
+}; // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
